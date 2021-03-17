a3baeca2c526a4764370dd1dc303c9490b6a1506
Glagan <Glagan@users.noreply.github.com>
2021-03-17 01:50:31 +0100

    Insensitive search in Headers

--- src/Camagru/Http/RequestInput.php
+++ src/Camagru/Http/RequestInput.php
@@ -7,17 +7,19 @@ class RequestInput
 	public function __construct(string $method, HeaderList $headers)
 	{
 		$this->values = [];
-		// Get and filter query params
-		$getInput = \filter_input_array(INPUT_GET, FILTER_DEFAULT, true);
-		if (\is_array($getInput)) {
-			foreach ($getInput as $name => $value) {
-				$this->values[$name] = $value;
-			}
-		}
 
-		if ($method != 'GET') {
+		// Get and filter query params
+		if ($method === 'GET') {
+			$getInput = \filter_input_array(INPUT_GET, FILTER_DEFAULT, true);
+			if (\is_array($getInput)) {
+				foreach ($getInput as $name => $value) {
+					$this->values[$name] = $value;
+				}
+			}
+		} else if ($method !== 'OPTIONS') {
 			// Add decoded JSON input
-			if ($headers->has(Header::CONTENT_TYPE, Header::JSON_TYPE)) {
+			$contentType = $headers->get(Header::CONTENT_TYPE);
+			if ($contentType !== false && \mb_strpos($contentType, Header::JSON_TYPE) === 0) {
 				$content = \file_get_contents('php://input');
 				if ($content != '') {
 					$bodyInput = \filter_var_array(\json_decode($content, true), FILTER_DEFAULT, true);
