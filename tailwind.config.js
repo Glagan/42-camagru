e468857066ba3722a27d91663ff595e885732acd
Glagan <Glagan@users.noreply.github.com>
2021-03-21 15:53:35 +0100

    Add Tailwind CSS Add dev commands

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	purge: ['./public/**/*.html', './resources/**/*.ts'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
