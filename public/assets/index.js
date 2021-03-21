b5f3564550613c20af9ddd928cb4cf75e6958315
Glagan <Glagan@users.noreply.github.com>
2021-03-21 13:06:02 +0100

    Compile with Rollup Rename assets files

---
+++ public/assets/index.js
@@ -0,0 +1,7 @@
+(function () {
+	'use strict';
+
+	console.log('bouh !');
+
+}());
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzIjpbXSwic291cmNlc0NvbnRlbnQiOltdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7In0=
