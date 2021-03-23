cdc1f7afb82c649c7cd43fbc9dcf628247fc332f
Glagan <Glagan@users.noreply.github.com>
2021-03-23 14:51:07 +0100

    Add Router and Application.navigate Add Components for all pages Update Navigation Extract Theme function to it's own file

---
+++ resources/Components/PageNotFound.ts
@@ -0,0 +1,7 @@
+import { Component } from '../Component';
+
+export class PageNotFound extends Component {
+	create(): void {}
+	bind(): void {}
+	render(): void {}
+}
---
+++ resources/Components/SingleImage.ts
@@ -0,0 +1,7 @@
+import { Component } from '../Component';
+
+export class SingleImage extends Component {
+	create(): void {}
+	bind(): void {}
+	render(): void {}
+}
