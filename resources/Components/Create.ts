2992be7f7fd6c1b6eceac3fa13aa473d645a39bc
Glagan <Glagan@users.noreply.github.com>
2021-03-31 02:19:10 +0200

    Live camera preview in Create route Add disabled button style Fix some icons

--- resources/Components/Create.ts
+++ resources/Components/Create.ts
@@ -1,40 +1,48 @@
 import { Component } from '../Component';
+import { Alert } from '../UI/Alert';
 import { DOM } from '../Utility/DOM';
 
 export class Create extends Component {
 	static auth = true;
 
-	container!: HTMLElement;
-	preview!: HTMLImageElement;
+	preview!: HTMLElement;
+	noPreview!: HTMLElement;
+	videoPreview!: HTMLVideoElement;
+	imagePreview!: HTMLImageElement;
+	layers!: HTMLElement[];
 	actions!: HTMLElement;
 	sourceSelection!: HTMLElement;
-	enableCamera!: HTMLButtonElement;
-	upload!: HTMLButtonElement;
+	selectCamera!: HTMLButtonElement;
+	selectUpload!: HTMLButtonElement;
 	capture!: HTMLButtonElement;
+	cancelCapture!: HTMLButtonElement;
 	submit!: HTMLButtonElement;
 	decorations!: HTMLElement;
 
 	create(): void {
-		this.preview = DOM.create('img', {
-			className: 'shadow-md',
-			src: 'https://via.placeholder.com/900x450?text=Preview',
-			width: 900,
-			height: 450,
+		this.layers = [];
+		this.preview = DOM.create('div', {
+			className: 'sticky top-4 z-30 bg-gray-100 dark:bg-gray-600 flex items-center justify-center',
 		});
-		this.container = DOM.create('div', {
-			className: 'sticky top-4 z-30 bg-gray-100 dark:bg-gray-600',
-			childs: [this.preview],
-		});
-		this.enableCamera = DOM.button('primary', 'camera', 'Camera');
-		this.enableCamera.classList.add('rounded-tr-none', 'rounded-br-none');
-		this.upload = DOM.button('secondary', 'upload', 'Upload');
-		this.upload.classList.add('border-l-0', 'rounded-tl-none', 'rounded-bl-none');
-		this.sourceSelection = DOM.create('div', { childs: [this.enableCamera, this.upload] });
+		this.videoPreview = DOM.create('video', { className: 'shadow-md w-full', loop: true, volume: 0 });
+		this.imagePreview = DOM.create('img', { className: 'shadow-md w-full' });
+		this.selectCamera = DOM.button('primary', 'camera', 'Camera');
+		this.selectCamera.classList.add('rounded-tr-none', 'rounded-br-none');
+		this.selectUpload = DOM.button('secondary', 'upload', 'Upload');
+		this.selectUpload.classList.add('border-l-0', 'rounded-tl-none', 'rounded-bl-none');
+		this.sourceSelection = DOM.create('div', { childs: [this.selectCamera, this.selectUpload] });
 		this.capture = DOM.button('primary', 'camera', 'Capture');
+		this.capture.classList.add('mr-2', 'hidden');
+		this.cancelCapture = DOM.button('error', 'x-circle', 'Cancel');
+		this.cancelCapture.classList.add('hidden');
 		this.submit = DOM.button('success', 'plus-circle', 'Submit');
 		this.actions = DOM.create('div', {
 			className: 'flex flex-row flex-nowrap justify-between mt-2',
-			childs: [this.sourceSelection, this.capture, this.submit],
+			childs: [
+				this.sourceSelection,
+				DOM.create('div', { childs: [this.capture, this.cancelCapture] }),
+				this.submit,
+			],
 		});
 		this.decorations = DOM.create('div', {
 			className:
@@ -42,9 +50,78 @@ export class Create extends Component {
 		});
 	}
 
-	bind(): void {}
+	private enableCapture() {
+		this.capture.classList.remove('hidden');
+		this.cancelCapture.classList.add('hidden');
+	}
+
+	private enableCamera(): void {
+		DOM.clear(this.preview);
+		this.cancelCapture.classList.add('hidden');
+		this.preview.appendChild(
+			Alert.make('info', 'Allow the Camera permission on the top left to be able to use it.')
+		);
+		navigator.mediaDevices
+			.getUserMedia({ audio: false, video: true })
+			.then((stream) => {
+				DOM.clear(this.preview);
+				this.enableCapture();
+				this.preview.appendChild(this.videoPreview);
+				this.videoPreview.srcObject = stream;
+			})
+			.catch((error) => {
+				DOM.clear(this.preview);
+				this.cancelCapture.classList.add('hidden');
+				this.preview.appendChild(
+					Alert.make('danger', 'Could not access your Camera, please allow this page to use your Camera.')
+				);
+			});
+	}
+
+	private setCapturePreview(image: string): void {
+		this.imagePreview.src = image;
+		this.videoPreview.remove();
+		this.preview.appendChild(this.imagePreview);
+		this.capture.classList.add('hidden');
+		this.cancelCapture.classList.remove('hidden');
+		this.submit.disabled = false;
+	}
+
+	bind(): void {
+		this.videoPreview.addEventListener('loadedmetadata', (event) => {
+			this.videoPreview.play();
+		});
+		this.selectCamera.addEventListener('click', (event) => {
+			event.preventDefault();
+			this.enableCamera();
+		});
+		this.selectUpload.addEventListener('click', (event) => {
+			event.preventDefault();
+		});
+		this.capture.addEventListener('click', (event) => {
+			event.preventDefault();
+			const canvas = DOM.create('canvas');
+			const context = canvas.getContext('2d')!;
+			context.drawImage(this.videoPreview, 0, 0, canvas.width, canvas.height);
+			const image = canvas.toDataURL('image/png');
+			this.setCapturePreview(image);
+		});
+		this.cancelCapture.addEventListener('click', (event) => {
+			event.preventDefault();
+			this.imagePreview.src = '';
+			this.imagePreview.remove();
+			this.preview.appendChild(this.videoPreview);
+			this.videoPreview.play();
+			this.capture.classList.remove('hidden');
+			this.cancelCapture.classList.add('hidden');
+			this.submit.disabled = true;
+		});
+	}
 
 	render(): void {
-		DOM.append(this.parent, this.container, this.actions, this.decorations);
+		this.cancelCapture.classList.add('hidden');
+		this.submit.disabled = true;
+		DOM.append(this.parent, this.preview, this.actions, this.decorations);
+		this.enableCamera();
 	}
 }
