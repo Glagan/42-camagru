fff96e9d00771948e0ce945af9792cac490aca0d
Glagan <Glagan@users.noreply.github.com>
2021-04-14 00:39:15 +0200

    Add missing postcss-cli dependency Add some console output in start script

--- Docker/node/start.sh
+++ Docker/node/start.sh
@@ -4,15 +4,20 @@
 if [ ! -f "/app/storage/.initialized" ]; then
 	cd /app
 	# Build assets
+	echo 'Installing dependencies...'
 	npm install
+	echo 'Building assets...'
 	npm run build
 	# Create folder is they still don't exist somehow
 	mkdir -p /app/storage/decorations /app/storage/uploads
 	# Download decorations
+	echo 'Downloading Decorations...'
 	wget https://ramune.nikurasu.org/camagru/decorations.zip -P /tmp
 	unzip -o /tmp/decorations.zip -d /app/storage/decorations
 	# Download example uploads
+	echo 'Downloading Uploads...'
 	wget https://ramune.nikurasu.org/camagru/uploads.zip -P /tmp
 	unzip -o /tmp/uploads.zip -d /app/storage/uploads
 	touch /app/storage/.initialized
+	echo 'Done.'
 fi
