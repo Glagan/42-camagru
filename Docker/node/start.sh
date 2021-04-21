b412eac4653b3ff877eef811dfa2c0e74c088fe9
Glagan <Glagan@users.noreply.github.com>
2021-04-21 16:03:09 +0200

    Fix Exception in Single Creation endpoint Always rebuild assets in bootstrap container

--- Docker/node/start.sh
+++ Docker/node/start.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
+# Build assets -- always
+echo 'Installing dependencies...'
+echo 'This can take a long time !'
+npm install
+echo 'Building assets...'
+npm run build
+
 # First initialization if Decorations and uploads doesn't exist
 if [ ! -f "/app/storage/.initialized" ]; then
 	cd /app
-	# Build assets
-	echo 'Installing dependencies...'
-	echo 'This can take a long time !'
-	npm install
-	echo 'Building assets...'
-	npm run build
 	# Create folder is they still don't exist somehow
 	mkdir -p /app/storage/decorations /app/storage/uploads /app/storage/tmp /app/storage/logs
 	# Download decorations
@@ -20,5 +21,6 @@ if [ ! -f "/app/storage/.initialized" ]; then
 	wget -q --show-progress https://ramune.nikurasu.org/camagru/uploads.zip -P /tmp
 	unzip -o /tmp/uploads.zip -d /app/storage/uploads
 	touch /app/storage/.initialized
-	echo 'Done ! Available at http://localhost:8080'
 fi
+
+echo 'Done ! Available at http://localhost:8080'
